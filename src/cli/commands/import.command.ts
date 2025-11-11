48c5ad925aa991819671c289fd57edf76b2b8bae
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-11-11 15:29:33 +0200

    feat: read TSV file functionality

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,11 +1,31 @@
+import path from 'node:path';
+
 import { Command } from './command.interface.js';
+import { TSVFileReader } from '../../lib/TSVFileReader/TSVFileReader.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
-    return '--help';
+    return '--import';
   }
 
-  public execute() {
-    console.log('Execute call');
+  public execute(...args: string[]) {
+    try {
+      if (args.length === 0) {
+        throw new Error('No path for TSV file is provided');
+      }
+
+      const [fileArg] = args;
+      const filePath = path.resolve(fileArg);
+
+      const reader = new TSVFileReader(filePath);
+      reader.read();
+      console.log(reader.toOffersArray());
+    } catch (error: unknown) {
+      console.error('Failed to import file');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
---
+++ src/lib/TSVFileReader/TSVFileReader.ts
@@ -0,0 +1,55 @@
+import fs from 'node:fs';
+
+import { FileReader } from './FileReader.interface.js';
+
+export class TSVFileReader implements FileReader {
+  private rawData: string = '';
+  //  private rowsData: Array<Array<string>>;
+
+  constructor(private readonly path: string) {}
+
+  public read() {
+    if (!this.path) {
+      throw new Error('No path for TSV file is provided');
+    }
+
+    if (!fs.existsSync(this.path)) {
+      throw new Error(`TSV File not found at: ${this.path}`);
+      return;
+    }
+    this.rawData = fs.readFileSync(this.path, 'utf8');
+  }
+
+  public toOffersArray() {
+    if (!this.rawData) {
+      throw new Error('File was not read');
+    }
+    return this.rawData
+      .split('\n')
+      .filter((item) => item.length)
+      .map((line) => {
+        const values = line.split('\t');
+        return [
+          'title',
+          'description',
+          'publication_date',
+          'city',
+          'preview_image',
+          'property_photos',
+          'premium_flag',
+          'rating',
+          'property_type',
+          'number_of_rooms',
+          'number_of_guests',
+          'rental_cost',
+          'features',
+          'author',
+          'number_of_comments',
+          'coordinates',
+        ].reduce((acc, key, i) => {
+          acc[key] = values[i];
+          return acc;
+        }, {} as Record<string, string>);
+      });
+  }
+}
