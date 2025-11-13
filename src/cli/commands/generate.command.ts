678718da10f155c6bb3e3171493d4bae8c7f6397
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-11-13 23:33:47 +0200

    refactor: generate command, according to study project

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -4,7 +4,7 @@ import { generateErrorMessage } from '../../helpers/index.js';
 import { OfferGenerator } from '../../lib/OfferGenerator/index.js';
 import { Command } from './command.interface.js';
 import { MockServerData } from '../../types/mockServerData.js';
-import { TSVFileWriter } from '../../lib/TSVFileWriter/TSVFileWriter.js';
+import { TSVFileWriter } from '../../lib/TSVFileWriter/index.js';
 
 export class GenerateCommand implements Command {
   private rawData = {} as MockServerData;
@@ -25,9 +25,20 @@ export class GenerateCommand implements Command {
     }
   }
 
+  private async write(offerAmount: number, outputPath: string) {
+    const offerGenerator = new OfferGenerator(this.rawData);
+    const offerArr = [];
+    for (let i = 0; i < offerAmount; i++) {
+      const offer = offerGenerator.generate();
+      offerArr.push(offer);
+    }
+
+    const fileWriter = new TSVFileWriter(outputPath);
+    fileWriter.write(offerArr);
+  }
+
   public async execute(...args: string[]): Promise<void> {
     try {
-      console.log('generate called', { args });
       if (args.length === 0) {
         throw new Error('No args are provided');
       }
@@ -36,23 +47,10 @@ export class GenerateCommand implements Command {
       const offerAmount = Number.parseInt(amount, 10);
 
       await this.getData(url);
-      const offerGenerator = new OfferGenerator(this.rawData);
-      const offerArr = [];
-      for (let i = 0; i < offerAmount; i++) {
-        const offer = offerGenerator.generate();
-        offerArr.push(offer);
-      }
-
-      console.log({ offerAmount, outputPath });
-      const fileWriter = new TSVFileWriter(outputPath);
-      fileWriter.write(offerArr);
+      await this.write(offerAmount, outputPath);
+      console.info(`File ${outputPath} was created!`);
     } catch (error: unknown) {
       generateErrorMessage(error, 'Failed to generate mock TSV file');
     }
   }
 }
-
-//TODO: generate n offers
-//TODO: pass args to generate offers
-//TODO: error handling + try catch guarding, errors catchers
-//TODO: update import command call to work with big files
