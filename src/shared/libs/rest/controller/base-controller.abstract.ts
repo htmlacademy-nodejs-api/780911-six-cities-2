33af3771544a6c989e7467b98b2d02ed53473b4e
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-12-12 21:39:59 +0200

    feat: error handling

--- src/shared/libs/rest/controller/base-controller.abstract.ts
+++ src/shared/libs/rest/controller/base-controller.abstract.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'inversify';
 import { Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
 import { Logger } from '../../Logger/index.js';
 import { Controller } from './controller.interface.js';
 import { Route } from '../types/index.js';
@@ -19,7 +20,8 @@ export abstract class BaseController implements Controller {
   }
 
   public addRoute(route: Route) {
-    this._router[route.method](route.path, route.handler.bind(this));
+    const wrapperAsyncHandler = asyncHandler(route.handler.bind(this));
+    this._router[route.method](route.path, wrapperAsyncHandler);
     this.logger.info(
       `Route registered: ${route.method.toUpperCase()} ${route.path}`
     );
--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -3,3 +3,4 @@ export * from './controller/controller.interface.js';
 export * from './exception-filter/exception-filter.interface.js';
 export * from './exception-filter/app-exception-filter.js';
 export * from './types/index.js';
+export * from './errors/index.js';
